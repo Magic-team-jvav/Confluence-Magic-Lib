62801892fd694f60a97c8cba06d5c96bda83f4b0
westernat <westernat233@163.com>
2025-04-07 14:15:53 +0800

    将部分组件移到Lib

--- src/main/java/org/confluence/lib/ConfluenceMagicLib.java
+++ src/main/java/org/confluence/lib/ConfluenceMagicLib.java
@@ -1,6 +1,8 @@
 package org.confluence.lib;
 
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceType;
 import net.neoforged.bus.api.IEventBus;
@@ -9,6 +11,8 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.crafting.IngredientType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import org.confluence.lib.common.component.ModRarity;
+import org.confluence.lib.common.component.NbtComponent;
 import org.confluence.lib.common.recipe.AmountIngredient;
 import org.confluence.lib.common.worldgen.structure.GridPiece;
 import org.confluence.lib.common.worldgen.structure.SimpleTemplatePiece;
@@ -31,6 +35,9 @@ public class ConfluenceMagicLib {
     public static final Supplier<StructurePieceType.StructureTemplateType> SIMPLE_TEMPLATE_PIECE = PIECE_TYPES.register("simple_template_piece", () -> SimpleTemplatePiece::new);
     public static final Supplier<StructurePieceType.ContextlessType> GRID_PIECE = PIECE_TYPES.register("grid_piece", () -> GridPiece::new);
 
+    public static final DeferredRegister.DataComponents DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, TERRA_CURIO_ID);
+    public static final Supplier<DataComponentType<ModRarity>> MOD_RARITY = DATA_COMPONENT_TYPES.registerComponentType("mod_rarity", builder -> builder.persistent(ModRarity.CODEC).networkSynchronized(ModRarity.STREAM_CODEC));
+    public static final Supplier<DataComponentType<NbtComponent>> NBT = DATA_COMPONENT_TYPES.registerComponentType("nbt", builder -> builder.persistent(NbtComponent.CODEC).networkSynchronized(NbtComponent.STREAM_CODEC));
 
     public ConfluenceMagicLib(IEventBus modEventBus, ModContainer modContainer) {
         INGREDIENT_TYPES.register(modEventBus);
