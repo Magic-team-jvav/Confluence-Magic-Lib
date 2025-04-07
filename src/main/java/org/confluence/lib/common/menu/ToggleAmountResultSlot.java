3291fcf42a4814592ac2703a59286b0c1b8024b2
westernat <westernat233@163.com>
2025-04-07 14:55:36 +0800

    移动部分基类到Lib

---
+++ src/main/java/org/confluence/lib/common/menu/ToggleAmountResultSlot.java
@@ -0,0 +1,26 @@
+package org.confluence.lib.common.menu;
+
+import net.minecraft.world.Container;
+
+public class ToggleAmountResultSlot extends AmountResultSlot implements IToggleSlot {
+    public boolean isActive = true;
+
+    public ToggleAmountResultSlot(RecipeInputContainer input, Container result, int pSlot, int pX, int pY) {
+        super(input, result, pSlot, pX, pY);
+    }
+
+    @Override
+    public boolean isActive() {
+        return isActive;
+    }
+
+    @Override
+    public void setEnable(boolean enable) {
+        this.isActive = enable;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
