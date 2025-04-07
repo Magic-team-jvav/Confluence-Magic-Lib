3b47eac19c33507e92099f2d91002ce5dc41a675
westernat <westernat233@163.com>
2025-04-07 13:27:08 +0800

    将数量匹配合成表移到lib

--- src/main/java/org/confluence/lib/ConfluenceMagicLib.java
+++ src/main/java/org/confluence/lib/ConfluenceMagicLib.java
@@ -3,15 +3,25 @@ package org.confluence.lib;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.crafting.IngredientType;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import org.confluence.lib.recipe.AmountIngredient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Supplier;
 
 @Mod(ConfluenceMagicLib.MODID)
 public class ConfluenceMagicLib {
     public static final String MODID = "confluence_magic_lib";
     public static final Logger LOGGER = LoggerFactory.getLogger("Confluence Magic Lib");
 
-    public ConfluenceMagicLib(IEventBus modEventBus, ModContainer modContainer) {
+    public static final DeferredRegister<IngredientType<?>> INGREDIENT_TYPES = DeferredRegister.create(NeoForgeRegistries.Keys.INGREDIENT_TYPES, MODID);
+    public static final Supplier<IngredientType<AmountIngredient>> AMOUNT_INGREDIENT_TYPE = INGREDIENT_TYPES.register("amount_ingredient", () -> new IngredientType<>(AmountIngredient.CODEC, AmountIngredient.STREAM_CODEC));
 
+
+    public ConfluenceMagicLib(IEventBus modEventBus, ModContainer modContainer) {
+        INGREDIENT_TYPES.register(modEventBus);
     }
 }
