9650314b742860c6da456664d33f95aab2b4c36a
westernat <westernat233@163.com>
2025-04-19 12:23:12 +0800

    简化减速跳过mixin并移至lib

--- src/main/java/org/confluence/lib/common/LibTags.java
+++ src/main/java/org/confluence/lib/common/LibTags.java
@@ -8,6 +8,7 @@ import org.confluence.lib.ConfluenceMagicLib;
 public final class LibTags {
     public static class Items {
         public static final TagKey<Item> WIP = register("wip");
+        public static final TagKey<Item> SKIP_USING_SLOWDOWN = register("skip_using_slowdown");
 
         private static TagKey<Item> register(String id) {
             return ItemTags.create(ConfluenceMagicLib.lib(id));
