d5ddc3402093603f2213f628997a09621423e352
westernat <westernat233@163.com>
2025-04-24 14:15:35 +0800

    调手部长枪位置

--- src/main/java/org/confluence/lib/common/LibTags.java
+++ src/main/java/org/confluence/lib/common/LibTags.java
@@ -8,7 +8,8 @@ import org.confluence.lib.ConfluenceMagicLib;
 public final class LibTags {
     public static class Items {
         public static final TagKey<Item> WIP = register("wip");
-        public static final TagKey<Item> SKIP_USING_SLOWDOWN = register("skip_using_slowdown");
+        public static final TagKey<Item> SKIP_USING_SLOWDOWN = register("skip_using_slowdown"); // 使用时不影响玩家移动速度
+        public static final TagKey<Item> SKIP_RESET_STRENGTH = register("skip_reset_strength"); // 使用时不重置玩家攻击冷却
 
         private static TagKey<Item> register(String id) {
             return ItemTags.create(ConfluenceMagicLib.lib(id));
