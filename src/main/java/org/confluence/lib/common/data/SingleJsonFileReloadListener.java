b87a90be0890c2fe9aef4eb59b1a004afb6d586d
westernat <westernat233@163.com>
2025-06-27 15:25:23 +0800

    将mood_infos与dialogs转移

---
+++ src/main/java/org/confluence/lib/common/data/SingleJsonFileReloadListener.java
@@ -0,0 +1,60 @@
+package org.confluence.lib.common.data;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.neoforged.neoforge.resource.ContextAwareReloadListener;
+import org.confluence.lib.ConfluenceMagicLib;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public abstract class SingleJsonFileReloadListener extends ContextAwareReloadListener {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+    @Override
+    public final CompletableFuture<Void> reload(
+            PreparableReloadListener.PreparationBarrier stage,
+            ResourceManager resourceManager,
+            ProfilerFiller preparationsProfiler,
+            ProfilerFiller reloadProfiler,
+            Executor backgroundExecutor,
+            Executor gameExecutor
+    ) {
+        return CompletableFuture.supplyAsync(() -> prepare(resourceManager), backgroundExecutor).thenCompose(stage::wait).thenAcceptAsync(this::apply, gameExecutor);
+    }
+
+    protected Map<ResourceLocation, JsonElement> prepare(ResourceManager resourceManager) {
+        Map<ResourceLocation, JsonElement> map = new HashMap<>();
+        ResourceLocation resourceLocation = resourcePath();
+        for (Resource resource : resourceManager.getResourceStack(resourceLocation)) {
+            try (Reader reader = resource.openAsReader()) {
+                JsonObject jsonobject = GsonHelper.fromJson(GSON, reader, JsonObject.class);
+                for (Map.Entry<String, JsonElement> entry : jsonobject.entrySet()) {
+                    ResourceLocation loc = ResourceLocation.parse(entry.getKey());
+                    map.put(loc, entry.getValue());
+                }
+            } catch (RuntimeException | IOException ioexception) {
+                ConfluenceMagicLib.LOGGER.error("Couldn't {} read {} in data pack {}", identifier(), resourceLocation, resource.sourcePackId(), ioexception);
+            }
+        }
+        return map;
+    }
+
+    protected abstract void apply(Map<ResourceLocation, JsonElement> object);
+
+    protected abstract ResourceLocation resourcePath();
+
+    protected abstract String identifier();
+}
