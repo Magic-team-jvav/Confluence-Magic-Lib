38835fb0ddd92fba81878a7b1971d32c68d2f7f0
westernat <westernat233@163.com>
2025-07-15 11:27:04 +0800

    配方书进度

--- src/main/java/org/confluence/lib/common/recipe/AmountIngredient.java
+++ src/main/java/org/confluence/lib/common/recipe/AmountIngredient.java
@@ -57,18 +57,18 @@ public record AmountIngredient(Ingredient ingredient, int amount) implements ICu
     }
 
     public static Ingredient of(int amount, Ingredient ingredient) {
-        return new Ingredient(new AmountIngredient(ingredient, amount));
+        return new AmountIngredient(ingredient, amount).toVanilla();
     }
 
     public static Ingredient of(int amount, ItemLike... items) {
-        return new Ingredient(new AmountIngredient(Ingredient.of(items), amount));
+        return new AmountIngredient(Ingredient.of(items), amount).toVanilla();
     }
 
     public static Ingredient of(int amount, ItemStack... stacks) {
-        return new Ingredient(new AmountIngredient(Ingredient.of(stacks), amount));
+        return new AmountIngredient(Ingredient.of(stacks), amount).toVanilla();
     }
 
     public static Ingredient of(int amount, TagKey<Item> tag) {
-        return new Ingredient(new AmountIngredient(Ingredient.of(tag), amount));
+        return new AmountIngredient(Ingredient.of(tag), amount).toVanilla();
     }
 }
