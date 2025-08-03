4a77a386cd074676ead35538918159bb07df53da
MagicHarp <161911368+MagicHarp@users.noreply.github.com>
2025-08-03 20:49:46 +0800

    一些投掷物模型补充

--- src/main/java/org/confluence/lib/common/data/IdFixer.java
+++ src/main/java/org/confluence/lib/common/data/IdFixer.java
@@ -182,6 +182,7 @@ public class IdFixer {
             // 1.1.4 -> 1.1.5
             .put("terra_guns:blowpipe", "terra_guns:blowgun")
             .put("confluence:crystal_shards_item", "confluence:crystal_shards")
+            .put("confluence:throwing_knives", "confluence:throwing_knive")
             .build();
     private static final Map<String, String> BIOME_NAME_FIX_MAP = ImmutableMap.<String, String>builder()
             // 1.1.2 -> 1.1.3
