13058b84bf332230416d553cef05c4029f0ef483
westernat <westernat233@163.com>
2025-08-04 15:08:35 +0800

    id fixer改为使用事件注册

--- src/main/java/org/confluence/lib/common/data/IdFixer.java
+++ src/main/java/org/confluence/lib/common/data/IdFixer.java
@@ -19,8 +19,10 @@ import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
+import net.neoforged.fml.ModLoader;
 import org.apache.commons.lang3.mutable.MutableObject;
 import org.confluence.lib.ConfluenceMagicLib;
+import org.confluence.lib.api.NameFixRegisterEvent;
 import org.confluence.lib.util.LibUtils;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -63,133 +65,9 @@ public class IdFixer {
     public static final ResourceLocation FIXED_STP = ConfluenceMagicLib.asResource("simple_template_piece");
     public static final ResourceLocation GP = ResourceLocation.fromNamespaceAndPath(ConfluenceMagicLib.CONFLUENCE_ID, "grid_piece");
     public static final ResourceLocation FIXED_GP = ConfluenceMagicLib.asResource("grid_piece");
-    private static final Map<String, String> BLOCK_WITH_ITEM_NAME_FIX_MAP = ImmutableMap.<String, String>builder()
-            // 1.1.2 -> 1.1.3
-            .put("confluence:freeze_crate", "confluence:frozen_crate")
-            .put("confluence:ebony_stone", "confluence:ebonstone")
-            .put("confluence:pearl_stone", "confluence:pearlstone")
-            .put("confluence:tr_crimson_stone", "confluence:crimstone")
-            .put("confluence:ebony_cobblestone", "confluence:cobbled_ebonstone")
-            .put("confluence:pearl_cobblestone", "confluence:cobbled_pearlstone")
-            .put("confluence:tr_crimson_cobblestone", "confluence:cobbled_crimstone")
-            .put("confluence:ebony_sandstone", "confluence:ebonsandstone")
-            .put("confluence:tr_crimson_sandstone", "confluence:crimsandstone")
-            .put("confluence:pearl_sandstone", "confluence:pearlsandstone")
-            .put("confluence:ebony_sand", "confluence:ebonsand")
-            .put("confluence:pearl_sand", "confluence:pearlsand")
-            .put("confluence:crimson_sand", "confluence:crimsand")
-            .put("confluence:ebony_sand_layer_block", "confluence:ebonsand_layer_block")
-            .put("confluence:tr_crimson_sand_layer_block", "confluence:crimsand_layer_block")
-            .put("confluence:pearl_sand_layer_block", "confluence:pearlsand_layer_block")
-            .put("confluence:red_hardened_sand_block", "confluence:hardened_red_sand_block")
-            .put("confluence:ebony_hardened_sand_block", "confluence:hardened_ebonsand_block")
-            .put("confluence:pearl_hardened_sand_block", "confluence:hardened_pearlsand_block")
-            .put("confluence:tr_crimson_hardened_sand_block", "confluence:hardened_crimsand_block")
-            .put("confluence:ebony_moist_sand_block", "confluence:moistened_ebonsand_block")
-            .put("confluence:pearl_moist_sand_block", "confluence:moistened_pearlsand_block")
-            .put("confluence:tr_crimson_moist_sand_block", "confluence:moistened_crimsand_block")
-            .put("confluence:moist_sand_block", "confluence:moistened_sand_block")
-            .put("confluence:red_moist_sand_block", "confluence:moistened_red_sand_block")
-            .put("confluence:tr_lava_bricks", "confluence:hellstone_bricks")
-            .put("confluence:tr_amethyst_ore", "confluence:amethyst_ore")
-            .put("confluence:deepslate_tr_amethyst_ore", "confluence:deepslate_amethyst_ore")
-            .put("confluence:sanctification_tr_amethyst_ore", "confluence:sanctification_amethyst_ore")
-            .put("confluence:corruption_tr_amethyst_ore", "confluence:corruption_amethyst_ore")
-            .put("confluence:fleshification_tr_amethyst_ore", "confluence:fleshification_amethyst_ore")
-            .put("confluence:tr_crimson_ore", "confluence:crimtane_ore")
-            .put("confluence:deepslate_tr_crimson_ore", "confluence:deepslate_crimtane_ore")
-            .put("confluence:sanctification_tr_crimson_ore", "confluence:sanctification_crimtane_ore")
-            .put("confluence:corruption_tr_crimson_ore", "confluence:corruption_crimtane_ore")
-            .put("confluence:fleshification_tr_crimson_ore", "confluence:fleshification_crimtane_ore")
-            .put("confluence:raw_tr_crimson_block", "confluence:raw_crimtane_block")
-            .put("confluence:tr_crimson_block", "confluence:crimtane_block")
-            .put("confluence:tr_crimson_grass_block", "confluence:crimson_grass_block")
-            .put("confluence:tr_crimson_jungle_grass_block", "confluence:crimson_jungle_grass_block")
-            .put("confluence:tr_crimson_drooping_vine", "confluence:crimson_drooping_vine")
-            .put("confluence:tr_crimson_grass", "confluence:crimson_grass")
-            .put("confluence:tr_crimson_cattails_body", "confluence:crimson_cattails_body")
-            .put("confluence:tr_crimson_cattails_head", "confluence:crimson_cattails_head")
-            .put("confluence:tr_crimson_pot", "confluence:crimson_pot")
-            .put("confluence:tr_crimson_crate", "confluence:crimson_crate")
-            .put("confluence:tr_crimson_cattails", "confluence:crimson_cattails")
-            .put("confluence:tr_crimson_ore_bricks", "confluence:crimtane_ore_bricks")
-            .put("confluence:tr_crimson_rock_bricks", "confluence:crimstone_bricks")
-            .put("confluence:tr_amethyst_branches", "confluence:amethyst_branches")
-            .put("confluence:tr_amethyst_sapling", "confluence:amethyst_sapling")
-            .put("confluence:tr_amethyst_block", "confluence:amethyst_block")
-            .put("confluence:tr_polished_granite", "confluence:polished_granite")
-            .put("confluence:tr_copper_bricks", "confluence:copper_bricks")
-            .put("confluence:tr_gold_bricks", "confluence:golden_bricks")
-            .put("confluence:tr_iron_bricks", "confluence:iron_bricks")
-            .put("confluence:ebony_rock_bricks", "confluence:ebonstone_bricks")
-            .put("confluence:pearl_rock_bricks", "confluence:pearlstone_bricks")
-            .put("confluence:tr_obsidian_bricks", "confluence:obsidian_bricks")
-            .put("confluence:tr_obsidian_small_bricks", "confluence:obsidian_small_bricks")
-            .put("confluence:tr_smooth_obsidian", "confluence:smooth_obsidian")
-            .put("confluence:tr_oak_planks", "confluence:chiseled_oak_planks")
-            .put("confluence:tr_northland_planks", "confluence:chiseled_spruce_planks")
-            .put("confluence:tr_granite_column", "confluence:granite_column")
-            .put("confluence:tr_emerald_ore", "confluence:jade_ore")
-            .put("confluence:deepslate_tr_emerald_ore", "confluence:deepslate_jade_ore")
-            .put("confluence:sanctification_tr_emerald_ore", "confluence:sanctification_jade_ore")
-            .put("confluence:corruption_tr_emerald_ore", "confluence:corruption_jade_ore")
-            .put("confluence:fleshification_tr_emerald_ore", "confluence:fleshification_jade_ore")
-            .put("confluence:tr_emerald_block", "confluence:jade_block")
-            .put("confluence:emerald_branches", "confluence:jade_branches")
-            .put("confluence:emerald_sapling", "confluence:jade_sapling")
-            .put("confluence:emerald_chain", "confluence:jade_chain")
-            .build();
-    private static final Map<String, String> BLOCK_NAME_FIX_MAP = ImmutableMap.<String, String>builder()
-            // 1.1.2 -> 1.1.3
-            .put("confluence:copper_coin_pile", "confluence:copper_coin")
-            .put("confluence:silver_coin_pile", "confluence:silver_coin")
-            .put("confluence:golden_coin_pile", "confluence:golden_coin")
-            .put("confluence:platinum_coin_pile", "confluence:platinum_coin")
-            .put("confluence:emerald_coin_pile", "confluence:emerald_coin")
-            .putAll(BLOCK_WITH_ITEM_NAME_FIX_MAP)
-            .build();
-    private static final Map<String, String> ITEM_NAME_FIX_MAP = ImmutableMap.<String, String>builder()
-            // 1.1.2 -> 1.1.3
-            .put("confluence:copper_board_sword", "confluence:copper_broadsword")
-            .put("confluence:tin_board_sword", "confluence:tin_broadsword")
-            .put("confluence:lead_board_sword", "confluence:lead_broadsword")
-            .put("confluence:silver_board_sword", "confluence:silver_broadsword")
-            .put("confluence:tungsten_board_sword", "confluence:tungsten_broadsword")
-            .put("confluence:golden_board_sword", "confluence:golden_broadsword")
-            .put("confluence:platinum_board_sword", "confluence:platinum_broadsword")
-            .put("confluence:tr_crimson_ingot", "confluence:crimtane_ingot")
-            .put("confluence:raw_tr_crimson", "confluence:raw_crimtane")
-            .put("confluence:tr_emerald", "confluence:jade")
-            .put("terra_entity:emerald_whip", "terra_entity:jade_whip")
-            .put("confluence:emerald_minecart", "confluence:jade_minecart")
-            .put("confluence:emerald_hook", "confluence:jade_hook")
-            .put("confluence:emerald_staff", "confluence:jade_staff")
-            .put("confluence:tr_amethyst", "confluence:amethyst")
-            .put("confluence:tr_crimson_seed", "confluence:crimson_seed")
-            .put("confluence:tr_clownfish", "confluence:clownfish")
-            .put("confluence:tr_salmon", "confluence:salmon")
-            .put("confluence:red_light_saber", "confluence:red_phaseblade")
-            .put("confluence:orange_light_saber", "confluence:orange_phaseblade")
-            .put("confluence:yellow_light_saber", "confluence:yellow_phaseblade")
-            .put("confluence:green_light_saber", "confluence:green_phaseblade")
-            .put("confluence:blue_light_saber", "confluence:blue_phaseblade")
-            .put("confluence:purple_light_saber", "confluence:purple_phaseblade")
-            .put("confluence:white_light_saber", "confluence:white_phaseblade")
-            .put("confluence:demon_ocnch", "confluence:demon_conch")
-            .putAll(BLOCK_WITH_ITEM_NAME_FIX_MAP)
-            // 1.1.3 -> 1.1.4
-            .put("confluence:night_edge", "confluence:nights_edge")
-            // 1.1.4 -> 1.1.5
-            .put("terra_guns:blowpipe", "terra_guns:blowgun")
-            .put("confluence:crystal_shards_item", "confluence:crystal_shards")
-            .put("confluence:throwing_knives", "confluence:throwing_knive")
-            .build();
-    private static final Map<String, String> BIOME_NAME_FIX_MAP = ImmutableMap.<String, String>builder()
-            // 1.1.2 -> 1.1.3
-            .put("confluence:tr_crimson", "confluence:the_crimson")
-            .put("confluence:tr_crimson_desert", "confluence:the_crimson_desert")
-            .put("confluence:tr_crimson_tundra", "confluence:the_crimson_tundra")
-            .build();
+    private static Map<String, String> BLOCK_NAME_FIX_MAP;
+    private static Map<String, String> ITEM_NAME_FIX_MAP;
+    private static Map<String, String> BIOME_NAME_FIX_MAP;
     public static final Codec.ResultFunction<ResourceKey<Biome>> FIX_BIOME_KEY_FUNC = new Codec.ResultFunction<>() {
         @Override
         public <T> DataResult<Pair<ResourceKey<Biome>, T>> apply(DynamicOps<T> ops, T input, DataResult<Pair<ResourceKey<Biome>, T>> a) {
@@ -298,5 +176,25 @@ public class IdFixer {
                 return t;
             }
         };
+    }
+
+    public static void postRegisterEvents() {
+        ImmutableMap.Builder<String, String> blockWithItem = ImmutableMap.builder();
+        ModLoader.postEventWrapContainerInModOrder(new NameFixRegisterEvent.BlockWithItem(blockWithItem));
+        ImmutableMap<String, String> map = blockWithItem.build();
+
+        ImmutableMap.Builder<String, String> block = ImmutableMap.builder();
+        ModLoader.postEventWrapContainerInModOrder(new NameFixRegisterEvent.Block(block));
+        block.putAll(map);
+        BLOCK_NAME_FIX_MAP = block.build();
+
+        ImmutableMap.Builder<String, String> item = ImmutableMap.builder();
+        ModLoader.postEventWrapContainerInModOrder(new NameFixRegisterEvent.Item(item));
+        item.putAll(map);
+        ITEM_NAME_FIX_MAP = item.build();
+
+        ImmutableMap.Builder<String, String> biome = ImmutableMap.builder();
+        ModLoader.postEventWrapContainerInModOrder(new NameFixRegisterEvent.Biome(biome));
+        BIOME_NAME_FIX_MAP = biome.build();
     }
 }
