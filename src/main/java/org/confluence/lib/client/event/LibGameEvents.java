69be8f4e103baa0dcabc5cdaa13f6fc443db05d2
westernat <westernat233@163.com>
2025-08-04 22:02:40 +0800

    修lib不能单独染色稀有度的问题

--- src/main/java/org/confluence/lib/client/event/LibGameEvents.java
+++ src/main/java/org/confluence/lib/client/event/LibGameEvents.java
@@ -3,18 +3,40 @@ package org.confluence.lib.client.event;
 import com.mojang.datafixers.util.Either;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RenderTooltipEvent;
 import org.confluence.lib.ConfluenceMagicLib;
 import org.confluence.lib.common.LibTags;
+import org.confluence.lib.common.component.ModRarity;
+
+import java.util.List;
+import java.util.Optional;
 
 @EventBusSubscriber(modid = ConfluenceMagicLib.LIB_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public final class LibGameEvents {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
     public static void renderTooltip$GatherComponents(RenderTooltipEvent.GatherComponents event) {
-        if (event.getItemStack().isEmpty() || !event.getItemStack().is(LibTags.Items.WIP)) return;
-        event.getTooltipElements().add(1, Either.left(Component.translatable("tooltip.confluence.work_in_progress").withStyle(ChatFormatting.RED)));
+        ItemStack itemStack = event.getItemStack();
+        if (itemStack.isEmpty()) return;
+
+        List<Either<FormattedText, TooltipComponent>> tooltipElements = event.getTooltipElements();
+        if (tooltipElements.isEmpty()) return;
+        Optional<FormattedText> displayName = tooltipElements.getFirst().left();
+        if (displayName.isPresent() && displayName.get() instanceof Component component) {
+            ModRarity rarity = ModRarity.getRarity(itemStack);
+            if (rarity != null) {
+                tooltipElements.set(0, Either.left(component.copy().withColor(rarity.color())));
+            }
+        }
+
+        if (itemStack.is(LibTags.Items.WIP)) {
+            event.getTooltipElements().add(1, Either.left(Component.translatable("tooltip.confluence.work_in_progress").withStyle(ChatFormatting.RED)));
+        }
     }
 }
