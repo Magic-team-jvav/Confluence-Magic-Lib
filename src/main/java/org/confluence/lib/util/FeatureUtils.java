8828f940dbc8d2bd6a2fb19e599f2237c1f4662c
westernat <westernat233@163.com>
2025-08-09 13:14:37 +0800

    晶洞自动选择深板岩变种

--- src/main/java/org/confluence/lib/util/FeatureUtils.java
+++ src/main/java/org/confluence/lib/util/FeatureUtils.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import org.confluence.lib.ConfluenceMagicLib;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public final class FeatureUtils {
@@ -79,7 +80,20 @@ public final class FeatureUtils {
         for (int i = 0; i < 8; i++) {
             posCheck.set(centerPos.getX() + (x * ((i < 4) ? 1 : -1)), centerPos.getY() + (y * ((i % 4 < 2) ? 1 : -1)), centerPos.getZ() + (z * ((i % 2 < 1) ? 1 : -1)));
             if (replace || level.getBlockState(posCheck).isAir()) {
-                level.setBlock(posCheck.immutable(), blockState, 3);
+                level.setBlock(posCheck, blockState, 3);
+            }
+        }
+    }
+
+    public static void ball8(BlockPos.MutableBlockPos posCheck, boolean replace, int x, int y, int z, Function<BlockState, @Nullable BlockState> blockState, BlockPos centerPos, WorldGenLevel level) {
+        for (int i = 0; i < 8; i++) {
+            posCheck.set(centerPos.getX() + (x * ((i < 4) ? 1 : -1)), centerPos.getY() + (y * ((i % 4 < 2) ? 1 : -1)), centerPos.getZ() + (z * ((i % 2 < 1) ? 1 : -1)));
+            BlockState state = level.getBlockState(posCheck);
+            if (replace || state.isAir()) {
+                BlockState apply = blockState.apply(state);
+                if (apply != null) {
+                    level.setBlock(posCheck, apply, 3);
+                }
             }
         }
     }
@@ -89,9 +103,9 @@ public final class FeatureUtils {
             posCheck.set(centerPos.getX() + (x * ((i < 4) ? 1 : -1)), centerPos.getY() + (y * ((i % 4 < 2) ? 1 : -1)), centerPos.getZ() + (z * ((i % 2 < 1) ? 1 : -1)));
             if (replace || level.getBlockState(posCheck).isAir()) {
                 if (posCheck.getY() > checkY) {
-                    level.setBlock(posCheck.immutable(), blockState1, 3);
+                    level.setBlock(posCheck, blockState1, 3);
                 } else {
-                    level.setBlock(posCheck.immutable(), blockState2, 3);
+                    level.setBlock(posCheck, blockState2, 3);
                 }
             }
         }
@@ -102,14 +116,29 @@ public final class FeatureUtils {
             if (placePer >= random.nextFloat()) {
                 posCheck.set(centerPos.getX() + (x * ((i < 4) ? 1 : -1)), centerPos.getY() + (y * ((i % 4 < 2) ? 1 : -1)), centerPos.getZ() + (z * ((i % 2 < 1) ? 1 : -1)));
                 if (replace || level.getBlockState(posCheck).isAir()) {
-                    level.setBlock(posCheck.immutable(), blockState, 3);
+                    level.setBlock(posCheck, blockState, 3);
                 }
             }
         }
     }
 
-    //填充方法
-    //球体填充
+    public static void ball8(BlockPos.MutableBlockPos posCheck, boolean replace, int x, int y, int z, Function<BlockState, @Nullable BlockState> blockState, BlockPos centerPos, WorldGenLevel level, float placePer, RandomSource random) {
+        for (int i = 0; i < 8; i++) {
+            if (placePer >= random.nextFloat()) {
+                posCheck.set(centerPos.getX() + (x * ((i < 4) ? 1 : -1)), centerPos.getY() + (y * ((i % 4 < 2) ? 1 : -1)), centerPos.getZ() + (z * ((i % 2 < 1) ? 1 : -1)));
+                BlockState state = level.getBlockState(posCheck);
+                if (replace || state.isAir()) {
+                    BlockState apply = blockState.apply(state);
+                    if (apply != null) {
+                        level.setBlock(posCheck, apply, 3);
+                    }
+                }
+            }
+        }
+    }
+
+    // 填充方法
+    // 球体填充
     public static void ball(double radiusD, BlockPos centerPos, BlockState blockState, boolean replace, WorldGenLevel level) {
         int radius = (int) radiusD + 1;
         double radius2 = radiusD * radiusD;
@@ -129,7 +158,27 @@ public final class FeatureUtils {
         }
     }
 
-    //球体填充，带有指定y坐标上下不同种方块填充
+    // 球体填充，且允许根据原方块状态灵活调整新方块状态
+    public static void ball(double radiusD, BlockPos centerPos, Function<BlockState, @Nullable BlockState> blockState, boolean replace, WorldGenLevel level) {
+        int radius = (int) radiusD + 1;
+        double radius2 = radiusD * radiusD;
+        int x2;
+        int y2;
+        BlockPos.MutableBlockPos posCheck = centerPos.mutable();
+        for (int x = 0; x < radius; x++) {
+            x2 = x * x;
+            for (int y = 0; y < radius; y++) {
+                y2 = y * y;
+                for (int z = 0; z < radius; z++) {
+                    if ((x2 + y2 + z * z <= radius2)) {
+                        ball8(posCheck, replace, x, y, z, blockState, centerPos, level);
+                    }
+                }
+            }
+        }
+    }
+
+    // 球体填充，带有指定y坐标上下不同种方块填充
     public static void ball(double radiusD, BlockPos centerPos, BlockState blockState1, BlockState blockState2, boolean replace, WorldGenLevel level, int checkY) {
         int radius = (int) radiusD + 1;
         double radius2 = radiusD * radiusD;
@@ -149,7 +198,7 @@ public final class FeatureUtils {
         }
     }
 
-    //球体填充，带有随机比例
+    // 球体填充，带有随机比例
     public static void ball(double radiusD, BlockPos centerPos, BlockState blockState, boolean replace, WorldGenLevel level, float placePer, RandomSource random) {
         int radius = (int) radiusD + 1;
         double radius2 = radiusD * radiusD;
@@ -169,7 +218,27 @@ public final class FeatureUtils {
         }
     }
 
-    //立方体填充
+    // 球体填充，带有随机比例，且允许根据原方块状态灵活调整新方块状态
+    public static void ball(double radiusD, BlockPos centerPos, Function<BlockState, @Nullable BlockState> blockState, boolean replace, WorldGenLevel level, float placePer, RandomSource random) {
+        int radius = (int) radiusD + 1;
+        double radius2 = radiusD * radiusD;
+        int x2;
+        int y2;
+        BlockPos.MutableBlockPos posCheck = centerPos.mutable();
+        for (int x = 0; x < radius; x++) {
+            x2 = x * x;
+            for (int y = 0; y < radius; y++) {
+                y2 = y * y;
+                for (int z = 0; z < radius; z++) {
+                    if ((x2 + y2 + z * z <= radius2)) {
+                        ball8(posCheck, replace, x, y, z, blockState, centerPos, level, placePer, random);
+                    }
+                }
+            }
+        }
+    }
+
+    // 立方体填充
     public static void rectangular(BlockPos startPos, BlockPos endPos, BlockState blockstate, WorldGenLevel level, boolean replace) {
         int startX = Math.min(endPos.getX(), startPos.getX());
         int startY = Math.min(endPos.getY(), startPos.getY());
@@ -185,13 +254,13 @@ public final class FeatureUtils {
             for (int y = 0; y <= yLength; y++) {
                 for (int z = 0; z <= zLength; z++) {
                     posCheck.set(startX + x, startY + y, startZ + z);
-                    if (replace || level.getBlockState(posCheck.immutable()).canBeReplaced()) level.setBlock(posCheck.immutable(), blockstate, 3);
+                    if (replace || level.getBlockState(posCheck).canBeReplaced()) level.setBlock(posCheck, blockstate, 3);
                 }
             }
         }
     }
 
-    //椭球体填充
+    // 椭球体填充
     public static void ellipsoid(double radiusDX, double radiusDY, double radiusDZ, BlockPos centerPos, BlockState blockState, boolean replace, WorldGenLevel level) {
         int radiusX = (int) radiusDX + 1;
         int radiusY = (int) radiusDY + 1;
@@ -215,7 +284,7 @@ public final class FeatureUtils {
         }
     }
 
-    //立方体检查
+    // 立方体检查
     public static boolean rectangularCheck(BlockPos startPos, BlockPos endPos, WorldGenLevel level) {
         int startX = Math.min(endPos.getX(), startPos.getX());
         int startY = Math.min(endPos.getY(), startPos.getY());
@@ -232,7 +301,7 @@ public final class FeatureUtils {
             for (int y = 0; (y <= yLength) && bl; y++) {
                 for (int z = 0; (z <= zLength) && bl; z++) {
                     posCheck.set(startX + x, startY + y, startZ + z);
-                    if (!level.getBlockState(posCheck.immutable()).canBeReplaced()) bl = false;
+                    if (!level.getBlockState(posCheck).canBeReplaced()) bl = false;
                 }
             }
         }
