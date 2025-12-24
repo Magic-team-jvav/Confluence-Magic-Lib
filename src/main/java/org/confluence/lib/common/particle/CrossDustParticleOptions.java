272511210a16d844380669ac4232a8c0acf3d33c
voila <voila1106@gmail.com>
2025-12-25 03:03:33 +0800

    加点注释

--- src/main/java/org/confluence/lib/common/particle/CrossDustParticleOptions.java
+++ src/main/java/org/confluence/lib/common/particle/CrossDustParticleOptions.java
@@ -28,15 +28,15 @@ public final class CrossDustParticleOptions implements ParticleOptions {
     public static final int FLAG_PULSE = 0b10000;
 
     public static final MapCodec<CrossDustParticleOptions> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            Codec.INT.fieldOf("centerColor").forGetter(thisOptions -> thisOptions.centerColor),
-            Codec.INT.fieldOf("edgeColor").forGetter(thisOptions -> thisOptions.edgeColor),
-            ExtraCodecs.VECTOR3F.fieldOf("velocity").forGetter(thisOptions -> thisOptions.velocity),
-            ExtraCodecs.VECTOR4F.fieldOf("speedCurve").forGetter(thisOptions -> thisOptions.speedCurve),
-            Codec.FLOAT.fieldOf("scale").forGetter(thisOptions -> thisOptions.scale),
-            Codec.INT.fieldOf("lifetime").forGetter(thisOptions -> thisOptions.lifetime),
-            Codec.INT.fieldOf("roll").forGetter(thisOptions -> thisOptions.roll),
-            ExtraCodecs.VECTOR4F.fieldOf("rollCurve").forGetter(thisOptions -> thisOptions.rollCurve),
-            Codec.INT.fieldOf("flags").forGetter(thisOptions -> thisOptions.flags)
+        Codec.INT.fieldOf("centerColor").forGetter(thisOptions -> thisOptions.centerColor),
+        Codec.INT.fieldOf("edgeColor").forGetter(thisOptions -> thisOptions.edgeColor),
+        ExtraCodecs.VECTOR3F.fieldOf("velocity").forGetter(thisOptions -> thisOptions.velocity),
+        ExtraCodecs.VECTOR4F.fieldOf("speedCurve").forGetter(thisOptions -> thisOptions.speedCurve),
+        Codec.FLOAT.fieldOf("scale").forGetter(thisOptions -> thisOptions.scale),
+        Codec.INT.fieldOf("lifetime").forGetter(thisOptions -> thisOptions.lifetime),
+        Codec.INT.fieldOf("roll").forGetter(thisOptions -> thisOptions.roll),
+        ExtraCodecs.VECTOR4F.fieldOf("rollCurve").forGetter(thisOptions -> thisOptions.rollCurve),
+        Codec.INT.fieldOf("flags").forGetter(thisOptions -> thisOptions.flags)
     ).apply(instance, CrossDustParticleOptions::new));
 
     private static final StreamCodec<ByteBuf, Vector4f> VEC4F_CODEC = new StreamCodec<>() {
@@ -83,9 +83,13 @@ public final class CrossDustParticleOptions implements ParticleOptions {
     };
 
     public final boolean large;
-    /** argb */
+    /**
+     * argb
+     */
     public final int centerColor;
-    /** argb */
+    /**
+     * argb
+     */
     public final int edgeColor;
     public final Vector3f velocity;
     public final Vector4f speedCurve;
@@ -99,7 +103,21 @@ public final class CrossDustParticleOptions implements ParticleOptions {
     public final boolean pulse;
     public final int flags;
 
-
+    /**
+     * @param large          true则中心是2x2，否则为1x1
+     * @param centerColor    中心颜色，argb
+     * @param edgeColor      边缘颜色，argb
+     * @param velocity       初始速度
+     * @param speedCurve     速度曲线，贝塞尔控制点
+     * @param scale          初始大小
+     * @param lifetime       持续时间
+     * @param roll           初始每刻旋转角度
+     * @param rollCurve      旋转速度曲线，贝塞尔
+     * @param noGravity      无视重力
+     * @param noPhysics      没有碰撞
+     * @param fullBrightness 无视环境亮度，保持最高亮度
+     * @param pulse          先变大再变小
+     */
     public CrossDustParticleOptions(boolean large, int centerColor, int edgeColor, Vector3f velocity, Vector4f speedCurve, float scale, int lifetime, int roll, Vector4f rollCurve, boolean noGravity, boolean noPhysics, boolean fullBrightness, boolean pulse) {
         this.large = large;
         this.centerColor = centerColor;
@@ -115,14 +133,25 @@ public final class CrossDustParticleOptions implements ParticleOptions {
         this.fullBrightness = fullBrightness;
         this.pulse = pulse;
         int flag = 0;
-        if(large) flag |= FLAG_LARGE;
-        if(noGravity) flag |= FLAG_NO_GRAVITY;
-        if(noPhysics) flag |= FLAG_NO_PHYSICS;
-        if(fullBrightness) flag |= FLAG_FULL_BRIGHTNESS;
-        if(pulse) flag |= FLAG_PULSE;
+        if (large) flag |= FLAG_LARGE;
+        if (noGravity) flag |= FLAG_NO_GRAVITY;
+        if (noPhysics) flag |= FLAG_NO_PHYSICS;
+        if (fullBrightness) flag |= FLAG_FULL_BRIGHTNESS;
+        if (pulse) flag |= FLAG_PULSE;
         this.flags = flag;
     }
 
+    /**
+     * @param centerColor 中心颜色，argb
+     * @param edgeColor   边缘颜色，argb
+     * @param velocity    初始速度
+     * @param speedCurve  速度曲线，贝塞尔控制点
+     * @param scale       初始大小
+     * @param lifetime    持续时间
+     * @param roll        初始每刻旋转角度
+     * @param rollCurve   旋转速度曲线，贝塞尔
+     * @param flags       保存各个开关的flag，具体见各个FLAG_常量
+     */
     public CrossDustParticleOptions(int centerColor, int edgeColor, Vector3f velocity, Vector4f speedCurve, float scale, int lifetime, int roll, Vector4f rollCurve, int flags) {
         this.centerColor = centerColor;
         this.edgeColor = edgeColor;
