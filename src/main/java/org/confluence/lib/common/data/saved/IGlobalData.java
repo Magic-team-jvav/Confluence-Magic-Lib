e21b8ffce5abe868be5b65ebeccb27e1045785a9
westernat <westernat233@163.com>
2026-01-10 21:30:23 +0800

    重写HUD房屋选择器，并加上区域边框

--- src/main/java/org/confluence/lib/common/data/saved/IGlobalData.java
+++ src/main/java/org/confluence/lib/common/data/saved/IGlobalData.java
@@ -2,29 +2,32 @@ package org.confluence.lib.common.data.saved;
 
 import com.mojang.serialization.Dynamic;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-/**
- * 用于整个存档的数据，而非维度数据（SavedData）
- */
+/// 用于整个存档的数据，而非维度数据（SavedData）
 public interface IGlobalData {
     <T> void decode(Dynamic<T> tag);
+
+    default <T> CompoundTag convert(Dynamic<T> dynamic) {
+        Tag tag = dynamic.convert(NbtOps.INSTANCE).getValue();
+        return (CompoundTag) tag;
+    }
 
     void encode(CompoundTag tag);
 
     String serializeKey();
 
-    /**
-     * 退出存档时自动清空缓存
-     */
+    /// 退出存档时自动清空缓存
     default void clear() {}
 
     List<IGlobalData> DAT = new CopyOnWriteArrayList<>();
 
-    // 必须调用这个，不然无法调用序列化
+    /// 必须调用这个，不然无法调用序列化
     static void registerGlobalData(IGlobalData... data) {
         DAT.addAll(Arrays.asList(data));
     }
