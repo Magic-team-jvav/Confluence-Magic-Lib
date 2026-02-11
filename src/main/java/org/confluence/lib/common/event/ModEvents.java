ca8d1107f8c2c78682f5ac5ebec8a467d758be09
westernat <westernat233@163.com>
2026-02-11 12:23:56 +0800

    idfixer懒加载

--- src/main/java/org/confluence/lib/common/event/ModEvents.java
+++ src/main/java/org/confluence/lib/common/event/ModEvents.java
@@ -2,12 +2,10 @@ package org.confluence.lib.common.event;
 
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import org.confluence.lib.ConfluenceMagicLib;
-import org.confluence.lib.common.data.IdFixer;
 import org.confluence.lib.common.fluid.FluidBuilder;
 import org.confluence.lib.network.AttackDamagePacketS2C;
 import org.confluence.lib.network.SetEntityDataPacketS2C;
@@ -24,10 +22,5 @@ public final class ModEvents {
         PayloadRegistrar registrar = event.registrar("1");
         registrar.playToClient(SetEntityDataPacketS2C.TYPE, SetEntityDataPacketS2C.STREAM_CODEC, SetEntityDataPacketS2C::handle);
         registrar.playToClient(AttackDamagePacketS2C.TYPE, AttackDamagePacketS2C.STREAM_CODEC, AttackDamagePacketS2C::handle);
-    }
-
-    @SubscribeEvent
-    public static void fmlLoadComplete(FMLLoadCompleteEvent event) {
-        event.enqueueWork(IdFixer::postRegisterEvents);
     }
 }
