0504e102ebe18e07fe332b7653b8b62dbd276cc6
Lazy-Pillow-Minecraft <wsc1122330@163.com>
2026-03-23 11:43:27 +0800

    虛空群系

--- src/main/java/org/confluence/lib/color/IntegerRGB.java
+++ src/main/java/org/confluence/lib/color/IntegerRGB.java
@@ -9,6 +9,9 @@ public record IntegerRGB(int red, int green, int blue) {
     public static final IntegerRGB VOID_A = of(0x2c182a);
     public static final IntegerRGB VOID_B = of(0x3b2e6b);
     public static final IntegerRGB VOID_C = of(0x3c6f98);
+    public static final IntegerRGB VOID_WEAVE_A = of(0x8641f8);
+    public static final IntegerRGB VOID_WEAVE_B = of(0x6516e9);
+    public static final IntegerRGB VOID_WEAVE_C = of(0x4d57fb);
     public static final IntegerRGB BLACK = of(0x000000);
     public static final IntegerRGB GRAY = of(0x828282);
     public static final IntegerRGB WHITE = of(0xFFFFFF);
