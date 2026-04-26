dd25f4e6f550a5ee35da3710866d2bb3ffb38df1
westernat <westernat233@163.com>
2026-04-26 20:22:50 +0800

    分组功能2代

--- src/main/java/org/confluence/lib/util/WipNotDisplayOutput.java
+++ src/main/java/org/confluence/lib/util/WipNotDisplayOutput.java
@@ -2,6 +2,7 @@ package org.confluence.lib.util;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import org.confluence.lib.common.LibTags;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -20,6 +21,22 @@ public class WipNotDisplayOutput implements CreativeModeTab.Output {
         if (displayable(stack)) {
             delegate.accept(stack, tabVisibility);
         }
+    }
+
+    public void acceptAll(DeferredRegister.Items register, CreativeModeTab.TabVisibility tabVisibility) {
+        register.getEntries().forEach(item -> accept(item.get(), tabVisibility));
+    }
+
+    public void acceptAll(DeferredRegister.Items register) {
+        acceptAll(register, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+    }
+
+    public void acceptAll(DeferredRegister.Blocks register, CreativeModeTab.TabVisibility tabVisibility) {
+        register.getEntries().forEach(item -> accept(item.get(), tabVisibility));
+    }
+
+    public void acceptAll(DeferredRegister.Blocks register) {
+        acceptAll(register, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
     }
 
     public static boolean displayable(ItemStack stack) {
